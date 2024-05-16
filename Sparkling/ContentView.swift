3c21222dab91ee2503dc29f94b5973c3bc50cfbd
Maxim Vasilyev <mayxis@inbox.ru>
2024-05-16 15:09:19 +0300

    Initial Commit

---
+++ Sparkling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sparkling
+//
+//  Created by Maxim Vasilyev on 16.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sparkling/SparklingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparklingApp.swift
+//  Sparkling
+//
+//  Created by Maxim Vasilyev on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparklingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
